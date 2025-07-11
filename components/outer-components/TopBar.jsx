258eee0968acbb293aec578fe0d08f71bf093962
Markus Jarvis <markus.h.jarvis@gmail.com>
2025-07-11 13:06:26 -0500

    update TopBar component with new icons and enhance layout controls; modify globals.css for font and color variables

--- components/outer-components/TopBar.jsx
+++ components/outer-components/TopBar.jsx
@@ -1,6 +1,15 @@
 import { VscChromeMinimize } from "react-icons/vsc"
 import { IoCloseOutline } from "react-icons/io5"
 import { RiExpandLeftRightFill } from "react-icons/ri"
+import { GoArrowLeft } from "react-icons/go"
+import { GoArrowRight } from "react-icons/go"
+import { RxMagnifyingGlass } from "react-icons/rx"
+import { RiCopilotFill } from "react-icons/ri"
+import { IoChevronDown } from "react-icons/io5"
+import { VscLayout } from "react-icons/vsc"
+import { VscLayoutSidebarRight } from "react-icons/vsc"
+import { VscLayoutPanelOff } from "react-icons/vsc"
+import { VscLayoutSidebarRightOff } from "react-icons/vsc"
 
 export default function TopBar() {
 	return (
@@ -25,12 +34,30 @@ export default function TopBar() {
 					</span>
 				</div>
 			</div>
-			<div className="flex gap-4">
-				<div>arrows</div>
-				<div>search</div>
-				<div>chat</div>
+			{/* */}
+			{/* Search Icon, Arrows, Copilot Icon */}
+			{/* */}
+			<div className="flex items-center gap-4">
+				<GoArrowLeft className="text-xl text-gray-500 hover:text-gray-300 cursor-pointer" />
+				<GoArrowRight className=" text-xl text-gray-500 hover:text-gray-300 cursor-pointer" />
+				<div className="w-[30vw] h-5 flex items-center justify-center gap-1 bg-vs-gray-300 rounded-md">
+					<RxMagnifyingGlass className="text-lg text-gray-500 rotate-90" />
+					<p className="text-xs text-gray-300">portfolio</p>
+				</div>
+				<div className="flex items-center justify-center gap-1">
+					<RiCopilotFill className="text-gray-300 text-lg" />
+					<IoChevronDown className="text-gray-300 text-sm" />
+				</div>
 			</div>
-			<div className="">layouticons</div>
+			{/* */}
+			{/* Layout Icons */}
+			{/* */}
+			<div className="flex gap-3">
+				<VscLayout className="text-gray-300" />
+				<VscLayoutSidebarRight className="text-gray-300 rotate-180" />
+				<VscLayoutPanelOff className="text-gray-300" />
+				<VscLayoutSidebarRightOff className="text-gray-300" />
+			</div>
 		</div>
 	)
 }
