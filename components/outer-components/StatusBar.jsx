e18041a35b160e093f91f0e6552afdcaa922606c
Markus Jarvis <markus.h.jarvis@gmail.com>
2025-07-12 18:53:01 -0500

    update StatusBar component: add icons and structure for left and right sections

--- components/outer-components/StatusBar.jsx
+++ components/outer-components/StatusBar.jsx
@@ -1,5 +1,29 @@
+import {
+	VscRemote,
+	VscSourceControl,
+	VscSync,
+	VscError,
+	VscWarning,
+	VscHistory,
+	VscCopilot,
+	VscBroadcast,
+	VscCheck,
+	VscBellDot,
+} from "react-icons/vsc"
+
 export default function StatusBar() {
 	return (
-		<div className="h-6 min-w-full flex items-center justify-between bg-vs-dark-gray text-white rounded-b-md"></div>
+		<div className="h-6 min-w-full flex items-center justify-between bg-vs-dark-gray text-white rounded-b-md">
+			{/** */}
+			{/** Status Bar Left Section */}
+			{/** */}
+			<div className="h-full w-8 flex items-center justify-center gap-2 bg-vsblue-500">
+				<VscRemote className="text-sm text-white" />
+			</div>
+			{/** */}
+			{/** Status Bar Right Section */}
+			{/** */}
+			<div>test</div>
+		</div>
 	)
 }
